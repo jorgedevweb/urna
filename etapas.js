cbc458e369320bee1d31067b5eebe98eda419515
Jorge Luiz <jorge.luiz.1984@hotmail.com>
2023-07-20 19:33:50 -0300

    first commit

---
+++ etapas.js
@@ -0,0 +1,50 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '83111',
+                nome: 'Montgomery Burns',
+                partido: ' Usina Springfield',
+                fotos:[
+                    {url:'83111.png', legenda: 'Vereador'}
+                ]
+            },
+            {
+                numero: '67222',
+                nome: 'Marge Simpson',
+                partido: 'CABELO AZUL',
+                fotos:[
+                    {url:'67222.png', legenda: 'Vereador'}
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '48',
+                nome: 'Bart Simpson',
+                partido: 'Skate',
+                vice: 'Milhouse Van Houten',
+                fotos:[
+                {url:'48_2.png', legenda: 'Prefeito'},
+                {url:'48.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                numero: '58',
+                nome: 'Homer Simpson',
+                partido: 'DONUTS',
+                vice: 'Lisa Simpson',
+                fotos:[
+                {url:'58.png', legenda: 'Prefeito'},
+                {url:'58_2.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+        ]
+    }
+];
